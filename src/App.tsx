d2205f7cd805a9bd7c3ac628cba65a51215d3a53
Anderson Vieira <asvieira.br@gmail.com>
2023-01-19 18:04:27 -0300

    feat: create component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Header } from "./components/Header"
 import "./styles/global.css"
 
-function App() {
+export function App() {
 
   return (
-    <h1>Web</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+    </div>
   )
 }
-
-export default App
