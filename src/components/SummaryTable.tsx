fca6b67881b64703d90f86edafd368bf5e21e963
Anderson Vieira <asvieira.br@gmail.com>
2023-01-20 12:06:34 -0300

    feat: auto fill empty days

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -5,6 +5,9 @@ const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDaysFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 export function SummaryTable() {
   return (
     <div className=" w-full flex" >
@@ -20,6 +23,13 @@ export function SummaryTable() {
         {summaryDates.map((day) => {
           return (
             <HabitDay key={day.toString()} />
+          )
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, index)=>{
+          return (
+            <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed">      
+            </div> 
           )
         })}
         
